f34509d804d38539b3ff97b497953a30804141e8
Morghani <89870673+Morghani@users.noreply.github.com>
2025-03-27 14:47:39 +0100

    Update uppg5.c

--- uppg5.c
+++ uppg5.c
@@ -1,17 +1,12 @@
-/* 
-    Uppg5:
-    1) Läs in ett heltal från tangentbordet
-    2) skapa en multiplikationstabell för talet upp till 10 x talet
-    3) Skriv ut din gångertabell, ett tal per rad (så 10 rader)
-    OBS: Ingen övrig formatering eller text. Bara siffror och radbrytningar.
-*/
-
 #include <stdio.h>
 
+int main() {
+    int num;
+    scanf("%d", &num);
 
-int main(){
-
-    // Skriv din kod här i main
+    for (int i = 1; i <= 10; i++) {
+        printf("%d\n", i * num);
+    }
 
     return 0;
 }
