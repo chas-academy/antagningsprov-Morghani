31e1691241ce3002b802412c78683c559c7c5d96
Morghani <89870673+Morghani@users.noreply.github.com>
2025-03-27 14:43:41 +0100

    Update uppg1.c

--- uppg1.c
+++ uppg1.c
@@ -1,15 +1,8 @@
-/* 
-    Uppg1:
-    1) Läs in två heltal från tangentbordet
-    2) Summera heltalen
-    3) Skriv ut resultatet (bara siffror, ingen övrig text)
-*/
-
 #include <stdio.h>
 
-int main(){
-
-    // Skriv din kod här i main
-
+int main() {
+    int num1, num2;
+    scanf("%d %d", &num1, &num2);
+    printf("%d", num1 + num2);
     return 0;
 }
