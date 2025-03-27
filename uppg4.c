4215fbbb183ca4252a512384e843934bad7defb2
Morghani <89870673+Morghani@users.noreply.github.com>
2025-03-27 14:46:58 +0100

    Update uppg4.c

--- uppg4.c
+++ uppg4.c
@@ -1,17 +1,15 @@
-/* 
-    Uppg4:
-    1) Läs in en sträng från tangentbordet (max 20 tecken)
-    2) Ta fram det tredje tecknet i strängen (ex: "hejsan" -> 'j')
-    3) Om strängen är kortare än tre tecken, skriv ut "Too short"
-    4) Om strängen är minst tre tecken lång, skriv ut det tredje tecknet
-*/
-
 #include <stdio.h>
 #include <string.h>
 
-int main(){
+int main() {
+    char str[21]; // max 20 tecken + '\0'
+    scanf("%20s", str);
 
-    // Skriv din kod här i main
+    if (strlen(str) < 3) {
+        printf("Too short");
+    } else {
+        printf("%c", str[2]); // tredje tecknet (index 2)
+    }
 
     return 0;
 }
