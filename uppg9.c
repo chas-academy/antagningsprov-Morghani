e1408e390ec54a7faa7a981e98e7ce437e84114d
Morghani <89870673+Morghani@users.noreply.github.com>
2025-03-27 14:52:45 +0100

    Update uppg9.c

--- uppg9.c
+++ uppg9.c
@@ -1,25 +1,14 @@
-/* 
-    Uppg9:
-    Ibland när tal ska läsas in så får man dem som en sträng istället
-    för som ett tal. 
-    1) Läs in två strängar 
-    2) Båda strängarna kommer att vara tre tecken långa och bara
-    innehålla siffror, t ex: "123" och "456"
-    3) Omvandla strängarna till heltal
-    4) Addera talen
-    5) Skriv ut resultatet
-    Ex: 
-    In: "123" "456"
-    Ut: 579 (Bara detta tal skrivs ut)
-    Du får lägga till funktioner i koden utöver det som skrivs i main
-*/
-
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
+int main() {
+    char str1[4], str2[4]; 
+    scanf("%3s %3s", str1, str2);
 
-int main(){
+    int num1 = atoi(str1);
+    int num2 = atoi(str2);
+    int sum = num1 + num2;
 
-   // Skriv din kod här
-
+    printf("%d", sum);
+    return 0;
 }
