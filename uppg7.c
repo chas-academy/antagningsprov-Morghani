4a99c140d2de8b4370b5efae65e8e6f7090dfa51
Morghani <89870673+Morghani@users.noreply.github.com>
2025-03-27 14:50:14 +0100

    Update uppg7.c

--- uppg7.c
+++ uppg7.c
@@ -1,21 +1,8 @@
-/* 
-    Uppg7:
-    1) Skriv färdigt funktionen för BMI nedanför 
-    2) BMI = vikt / (längd * längd) 
-    3) Rör inte main() - funktionen, den är redan klar
-    OBS: Du får inte använda math.h i den här uppgiften.
-*/
-
 #include <stdio.h>
 
-float BMI(float weight, float height){
-
-// Skriv din kod här
-
+float BMI(float weight, float height) {
+    return weight / (height * height);
 }
-
-
-
 
 // ---- OBS! Du får INTE modifiera koden i main() ----
 int main(){
